a038c7e040ffa2b1b8f1693822d732b4c17c3745
Ronald Stengel <rstengel@outlook.com>
2017-10-19 12:42:56 -0700

    ..

--- MiddleMeeter/MiddleMeeter/ImagePage.cs
+++ MiddleMeeter/MiddleMeeter/ImagePage.cs
@@ -3,24 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 using Xamarin.Forms;
-//using static System.Net.Mime.MediaTypeNames;
 
 namespace MiddleMeeter
 {
-    class SearchPage : ContentPage
+    class ImagePage : ContentPage
     {
-        public SearchPage()
+        public ImagePage()
         {
-            //BackgroundColor = Color.Black;
-            Title = "Game Release Countdown";
-            Padding = 20;
-
-            var button1 = new Button { Text = "Search...", HorizontalOptions = LayoutOptions.End, WidthRequest=200 };
+            var button1 = new Button { Text = "Jason ALexander" };
             button1.Clicked += Button1_Clicked;
-
-            // Test Button to test page
-            var button2 = new Button { Text = "Image Page...", HorizontalOptions = LayoutOptions.End, WidthRequest = 200 };
-            button2.Clicked += Button2_Clicked;
 
             var picker = new Picker { Title = "Mode" };
             foreach (var mode in new string[] { "Coffee", "Food", "Drinks" })
@@ -28,33 +19,29 @@ namespace MiddleMeeter
                 picker.Items.Add(mode);
             }
 
-            Label header = new Label
-            {
-                Text = "TableView for a form",
-                FontSize = 30,
-                FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
-            };
+            Title = "Image Page";
 
             TableView tableView = new TableView
             {
                 Intent = TableIntent.Form,
-                Root = new TableRoot("TableView Title")
+                Root = new TableRoot
                 {
-                    new TableSection("A JCALEX APP...")
+                     new TableSection("A JCALEX APP...")
                     {
-                        new ImageCell
+                        new ImageCell()
                         {
+                            Height = 150,
+                            
                             // This is the call to method getSource() 
                             ImageSource = getSource(),
-                            
+
                             Text = "Call of Duty - WWII",
                             Detail = "This is a little detail of the information...",
                         },
                     },
+
                 }
             };
-
             // Build the page.
             this.Content = new StackLayout
             {
@@ -63,6 +50,7 @@ namespace MiddleMeeter
                     //header,
                     tableView,
 
+                    
                     // Move this to its own label ////////////////////////////////////////////
                     new Label { Text = "Your  Location"},
                     new Entry { Placeholder = "Your location"},
@@ -72,29 +60,14 @@ namespace MiddleMeeter
                     picker,
 
                     button1,
-                    button2,
+                    //button2,
                 }
             };
-
-
         }
 
-        private void Button2_Clicked(object sender, EventArgs e)
+        private void Button1_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new ImagePage());
-
-            //throw new NotImplementedException();
-        }
-
-        // related to button1 above and what to do when pressed
-        void Button1_Clicked(object sender, EventArgs e)
-        {
-            var results = new Result[] {
-                new MiddleMeeter.Result { Name = "Starbucks", Description = "Some Coffee" },
-                new MiddleMeeter.Result { Name = "Seattle Tea", Description = "Some moew Coffee" } ,
-                new MiddleMeeter.Result { Name = "Dutch Brothers", Description = "Other kind of Coffee" },
-            };
-            Navigation.PushAsync(new ResultsPage(results));
+            this.BackgroundColor = Color.Firebrick;
         }
 
         // Method to get the format to retreive the image for Platform specific detaisl
@@ -105,9 +78,17 @@ namespace MiddleMeeter
                 case Device.iOS:
                     return ImageSource.FromUri(new Uri("https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png"));
                 case Device.Android:
-                    //return ImageSource.FromUri(new Uri("https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png"));
-                    //return ImageSource.FromFile("waterfront.jpg");
+                    
+                    WidthRequest = 600;
+                    HeightRequest = 600;
+                    MinimumHeightRequest = 600;
+                    MinimumWidthRequest = 600;
                     return ImageSource.FromFile("Icon.png");
+
+                    //VerticalOptions = LayoutOptions.End;
+                    //HorizontalOptions = LayoutOptions.End;
+                    //Aspect = Aspect.AspectFill;         //.AspectFit//.Fill  
+                    
                 case Device.WinPhone:
                     return ImageSource.FromFile("Images/waterfront.jpg");
                 default:
